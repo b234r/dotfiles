c0da93e2b8affdcd0e4e40febd24d1a54fe022aa
John <subscribe+gh@b234r.net>
2025-01-19 12:49:05 -0700

    fix(nvim): improve treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,51 +3,90 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
   config = function()
-    -- import nvim-treesitter plugin
     local treesitter = require("nvim-treesitter.configs")
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
+    treesitter.setup({
       ensure_installed = {
-        "json",
-        "yaml",
+        "bash",
+        "c",
+        "cpp",
+        "csv",
+        "dockerfile",
+        "git_config",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "gomod",
+        "gosum",
+        "gotmpl",
+        "helm",
+        "graphql",
         "html",
+        "javascript",
+        "json",
+        "just",
+        "lua",
+        "make",
         "markdown",
         "markdown_inline",
-        "graphql",
-        "bash",
-        "lua",
-        "vim",
-        "dockerfile",
-        "gitignore",
-        "vimdoc",
-        "go",
-        "rust",
         "php",
-        "javascript",
+        "psv",
+        "python",
+        "rust",
+        "sql",
         "typescript",
-        "c",
-        "cpp"
+        "terraform",
+        "toml",
+        "tsv",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
       },
+
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = true,
+      
+      highlight = {
+        enable = true,
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+      },
+      
+      indent = {
+        enable = true
+      },
+
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<leader>y",
-          node_incremental = "<leader>y",
-          scope_incremental = false,
-          node_decremental = "<bs>",
+          init_selection = "gnn", -- set to `false` to disable one of the mappings
+          node_incremental = "gni",
+          node_decremental = "gnd",
         },
       },
     })
+
+    vim.filetype.add({
+      extension = {
+        gotmpl = 'gotmpl',
+      },
+      pattern = {
+        [".*/templates/.*%.tpl"] = "helm",
+        [".*/templates/.*%.ya?ml"] = "helm",
+        ["helmfile.*%.ya?ml"] = "helm",
+      },
+    })
+
+    -- treesitter for folding
+    vim.wo.foldmethod = 'expr'
+    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
   end,
 }
