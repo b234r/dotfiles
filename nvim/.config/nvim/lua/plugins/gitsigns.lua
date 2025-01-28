3783cccfbf451f4f70ba4cebe106e3b06561142e
John <subscribe+gh@b234r.net>
2025-01-27 22:31:47 -0700

    feat(nvim): rework gitsigns and add hydra for git

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,53 +1,155 @@
 return {
   "lewis6991/gitsigns.nvim",
   config = function()
-    -- import lspconfig plugin
-    local gs = require("gitsigns")
-    gs.setup{
-      on_attach = function(bufnr)
-        local gitsigns = require('gitsigns')
+    local gitsigns = require("gitsigns")
 
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
+    gitsigns.setup({
+      signcolumn = false,
+      numhl = true,
+      attach_to_untracked = true,
+    })
 
-        -- Navigation
-        map('n', ']g', function()
-          if vim.wo.diff then
-            vim.cmd.normal({']g', bang = true})
-          else
-            gitsigns.nav_hunk('next')
-          end
-        end, {desc="Goto next git hunk"})
+    local map = vim.keymap.set
 
-        map('n', '[g', function()
-          if vim.wo.diff then
-            vim.cmd.normal({'[g', bang = true})
-          else
-            gitsigns.nav_hunk('prev')
-          end
-        end, {desc="Goto previous git hunk"})
-
-        -- Actions
-        map('n', '<leader>gs', gitsigns.stage_hunk, {desc="Stage hunk"})
-        map('n', '<leader>gr', gitsigns.reset_hunk, {desc="Reset hunk"})
-        map('n', '<leader>gu', gitsigns.undo_stage_hunk, {desc="Undo stage hunk"})
-        map('n', '<leader>gp', gitsigns.preview_hunk, {desc="Show hunk changes"})
-        map('v', '<leader>gs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end, {desc="Stage hunk"})
-        map('v', '<leader>gr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end, {desc="Reset hunk"})
-        map('n', '<leader>gS', gitsigns.stage_buffer, {desc="Stage buffer"})
-        map('n', '<leader>gR', gitsigns.reset_buffer, {desc="Reset buffer"})
-        map('n', '<leader>gb', function() gitsigns.blame_line{full=true} end, {desc="Show git blame"})
-        map('n', '<leader>gB', gitsigns.toggle_current_line_blame, {desc="Toggle git blame line"})
-        map('n', '<leader>gd', gitsigns.diffthis)
-        map('n', '<leader>gD', function() gitsigns.diffthis('~') end)
-        map('n', '<leader>gd', gitsigns.toggle_deleted)
-
-        -- Text object
-        map({'o', 'x'}, 'gh', ':<C-U>Gitsigns select_hunk<CR>')
+    map("n", "]g", function()
+      if vim.wo.diff then
+        vim.cmd.normal({ "]c", bang = true })
+      else
+        gitsigns.nav_hunk("next")
       end
-    }
+    end)
+
+    map("n", "[g", function()
+      if vim.wo.diff then
+        vim.cmd.normal({ "[c", bang = true })
+      else
+        gitsigns.nav_hunk("prev")
+      end
+    end)
+
+    map({ "o", "x" }, "gh", ":<C-U>Gitsigns select_hunk<CR>")
+
+    local Hydra = require("hydra")
+    local cmd = require("hydra.keymap-util").cmd
+
+    local git_stage = function()
+      local mode = vim.api.nvim_get_mode().mode:sub(1, 1)
+      if mode == "V" then                  -- visual-line mode
+        local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
+        vim.api.nvim_feedkeys(esc, "x", false) -- exit visual mode
+        vim.cmd("'<,'>Gitsigns stage_hunk")
+      else
+        vim.cmd("Gitsigns stage_hunk")
+      end
+    end
+
+    Hydra({ -- *hunk operations only work when gitsigns attaches to buffer
+      name = "Git",
+      hint = [[
+_]_: next hunk    _u_: undo stage     _O_: open browser        _l_: log for file _c_: status
+_[_: prev hunk    _r_: reset hunk     _Y_: copy url            _L_: log
+_S_: stage buffer _p_: preview hunk   _b_: toggle inline blame _d_: diff
+_s_: stage hunk   _x_: toggle deleted _B_: show blame          _D_ diff head
+
+                 _<Enter>_: Neogit  _<Esc>_: exit
+]],
+      config = {
+        color = "red",
+        invoke_on_body = true,
+        hint = {
+          float_opts = {
+            border = "rounded",
+          },
+        },
+      },
+      mode = { "n", "x" },
+      body = "<leader>g",
+      heads = {
+        {
+          "]",
+          function()
+            gitsigns.nav_hunk("next")
+            return "<Ignore>"
+          end,
+          { desc = "next hunk" },
+        },
+        {
+          "[",
+          function()
+            gitsigns.nav_hunk("prev")
+            return "<Ignore>"
+          end,
+          { desc = "prev hunk" },
+        },
+        { "S", gitsigns.stage_buffer,        { exit = true, desc = "stage buffer" } },
+        { "s", git_stage,                    { exit = true, desc = "stage hunk" } },
+        { "u", gitsigns.undo_stage_hunk,     { exit = true, desc = "undo last stage" } },
+        { "r", gitsigns.reset_hunk,          { exit = true, desc = "reset hunk" } },
+        { "x", gitsigns.toggle_deleted,      { exit = true, desc = "toggle deleted" } },
+        { "p", gitsigns.preview_hunk_inline, { exit = true, desc = "preview hunk" } },
+        {
+          "O",
+          function()
+            Snacks.gitbrowse()
+          end,
+          { exit = true, desc = "Git Browse" },
+        },
+        {
+          "Y",
+          function()
+            Snacks.gitbrowse({
+              open = function(url)
+                vim.fn.setreg("+", url)
+              end,
+              notify = false,
+            })
+          end,
+          { exit = true, desc = "Git Browse (copy)" },
+        },
+        { "b", gitsigns.toggle_current_line_blame, { exit = true, desc = "blame" } },
+        {
+          "B",
+          function()
+            gitsigns.blame_line({ full = true })
+          end,
+          { desc = "blame show full" },
+        },
+        {
+          "l",
+          function()
+            Snacks.lazygit.log_file()
+          end,
+          { exit = true, desc = "file git history" },
+        },
+        {
+          "L",
+          function()
+            Snacks.lazygit.log()
+          end,
+          { exit = true, desc = "Lazygit Log (cwd)" },
+        },
+        { "d", gitsigns.diffthis,                  { exit = true, desc = "Lazygit Log (cwd)" } },
+        {
+          "D",
+          function()
+            gitsigns.diffthis("~")
+          end,
+          { exit = true, desc = "Lazygit Log (cwd)" },
+        },
+        { "c", cmd("FzfLua git_status"), { exit = true, desc = "blame" } },
+        {
+          "<Enter>",
+          function()
+            Snacks.lazygit()
+          end,
+          { exit = true, desc = "Lazygit" },
+        },
+        {
+          "<Esc>",
+          nil,
+          { exit = true, nowait = true, desc = "exit" },
+        },
+      },
+    })
   end,
 }
