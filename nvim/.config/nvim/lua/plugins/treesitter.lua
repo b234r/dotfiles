8dcd08a284e7eb19e33403963cf4447f784143dd
John <subscribe+gh@b234r.net>
2025-02-21 21:06:15 -0700

    feat(nvim): add treesitter context and text objects

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-context",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
   event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
   config = function()
@@ -49,7 +53,7 @@ return {
       -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
-      
+
       highlight = {
         enable = true,
 
@@ -59,7 +63,7 @@ return {
         -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
       },
-      
+
       indent = {
         enable = true
       },
@@ -67,11 +71,62 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "gnn", -- set to `false` to disable one of the mappings
-          node_incremental = "gni",
-          node_decremental = "gnd",
+          init_selection = "<CR>", -- set to `false` to disable one of the mappings
+          node_incremental = "<CR>",
+          node_decremental = "<BS>",
         },
       },
+
+      textobjects = {
+        enable = true,
+        lsp_interop = {
+          enable = false,
+        },
+        move = {
+          enable = true,
+          set_jumps = true, -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]m"] = "@function.outer",
+            ["]c"] = "@class.outer"
+          },
+          goto_next_end = {
+            ["]M"] = "@function.outer",
+            ["]C"] = "@class.outer"
+          },
+          goto_previous_start = {
+            ["[m"] = "@function.outer",
+            ["[c"] = "@class.outer"
+          },
+          goto_previous_end = {
+            ["[M"] = "@function.outer",
+            ["[C"] = "@class.outer"
+          }
+        },
+        select = {
+          enable = true,
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["aC"] = "@class.outer",
+            ["iC"] = "@class.inner",
+            ["ac"] = "@conditional.outer",
+            ["ic"] = "@conditional.inner",
+            ["ae"] = "@block.outer",
+            ["ie"] = "@block.inner",
+            ["al"] = "@loop.outer",
+            ["il"] = "@loop.inner",
+            ["is"] = "@statement.inner",
+            ["as"] = "@statement.outer",
+            ["ad"] = "@comment.outer",
+            ["am"] = "@call.outer",
+            ["im"] = "@call.inner",
+          }
+        },
+        swap = {
+          enable = false,
+        }
+      }
     })
 
     vim.filetype.add({
@@ -88,5 +143,16 @@ return {
     -- treesitter for folding
     vim.wo.foldmethod = 'expr'
     vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+
+    -- enable context
+    require 'treesitter-context'.setup {
+      max_lines = 10,          -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 40,  -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      multiline_threshold = 1, -- Maximum number of lines to show for a single context
+    }
+
+    vim.api.nvim_set_hl(0, 'TreesitterContext', { bg = "None" })
+    vim.api.nvim_set_hl(0, 'TreesitterContextBottom', { underline = true, sp = "#3b4261" })
+    vim.api.nvim_set_hl(0, 'TreesitterContextLineNumberBottom', { underline = true, sp = "#3b4261" })
   end,
 }
