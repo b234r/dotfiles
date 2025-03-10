f1ab54367c3bdaa4c84d165cd6ffd5e19ecd419b
John <subscribe+gh@b234r.net>
2025-03-09 21:07:47 -0700

    chore(nvim): cleanup trouble config

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,24 +1,28 @@
 return {
-  "folke/trouble.nvim",
-  dependencies = { "mbbill/undotree" },
-  cmd = "Trouble",
-  opts = {},
-  init = function()
-    vim.diagnostic.config({
-      virtual_text = false,
-      signs = {
-        text = {
-          [vim.diagnostic.severity.ERROR] = " ",
-          [vim.diagnostic.severity.WARN] = " ",
-          [vim.diagnostic.severity.INFO] = "󰠠 ",
-          [vim.diagnostic.severity.HINT] = " ",
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    opts = {},
+    init = function()
+      vim.diagnostic.config({
+        virtual_text = false,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = " ",
+            [vim.diagnostic.severity.WARN] = " ",
+            [vim.diagnostic.severity.INFO] = "󰠠 ",
+            [vim.diagnostic.severity.HINT] = " ",
+          },
         },
-      },
-    })
+      })
 
-    vim.g.undotree_WindowLayout = 2;
+      vim.g.undotree_WindowLayout = 2;
 
-    -- keymaps
-    vim.keymap.set("n", "<leader>x", vim.diagnostic.open_float, {})
-  end,
+      -- keymaps
+      vim.keymap.set("n", "<leader>x", vim.diagnostic.open_float, {})
+    end,
+  },
+  {
+    "mbbill/undotree",
+  },
 }
