76a7cb1ead45fe9d9a60fa357668815fd6da55bd
John <subscribe+gh@b234r.net>
2025-03-09 21:14:26 -0700

    feat(nvim): add scroll plugin

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,5 +1,22 @@
 return {
-  { "stevearc/dressing.nvim", event = "VeryLazy", },
-  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  { "christoomey/vim-tmux-navigator" }, -- tmux & split window navigation
+  {
+    "petertriho/nvim-scrollbar",
+    config = function(_, opts)
+      local colors = require("tokyonight.colors").setup()
+      require("scrollbar").setup({
+        handle = {
+          color = colors.bg_highlight,
+        },
+        marks = {
+          Search = { color = colors.orange },
+          Error = { color = colors.error },
+          Warn = { color = colors.warning },
+          Info = { color = colors.info },
+          Hint = { color = colors.hint },
+          Misc = { color = colors.purple },
+        }
+      })
+    end
+  },
 }
